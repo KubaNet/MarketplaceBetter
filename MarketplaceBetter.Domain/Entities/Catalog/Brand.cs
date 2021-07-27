f3a8694b71c599e29ad340f9eb874b2d2c51318f
Jakub Sadkowski <admin@mbetter.pl>
2021-07-27 20:02:05 +0200

    Refactoring dialogs

--- MarketplaceBetter.Domain/Entities/Catalog/Brand.cs
+++ MarketplaceBetter.Domain/Entities/Catalog/Brand.cs
@@ -11,5 +11,10 @@ namespace MarketplaceBetter.Domain.Entities.Catalog
         public string Name { get; set; }
 
         public string Code { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
--- MarketplaceBetter.Domain/Entities/Catalog/Product.cs
+++ MarketplaceBetter.Domain/Entities/Catalog/Product.cs
@@ -15,5 +15,10 @@ namespace MarketplaceBetter.Domain.Entities.Catalog
         public long BrandId { get; set; }
 
         public Brand Brand { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
