1f8ac0b3764a92e45f10b53ea2032fe17a86b28d
Kuba <jakub@mbetter.pl>
2021-08-09 19:34:15 +0200

    Keyword analysis

---
+++ MarketplaceBetter.Domain/Model/Amazon/MainPhraseModel.cs
@@ -0,0 +1,18 @@
+﻿using MarketplaceBetter.Domain.Entities.Amazon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceBetter.Domain.Model.Amazon
+{
+    public class MainPhraseModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public MainPhraseStatusModel Status { get; set; }
+    }
+}
---
+++ MarketplaceBetter.Services/Domain/Amazon/KeywordAnalysisService.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using MarketplaceBetter.Domain.Entities.Amazon;
+using MarketplaceBetter.Domain.Model.Amazon;
+using MarketplaceBetter.Infrastructure.Data;
+using MarketplaceBetter.Services.Domain.Amazon.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceBetter.Services.Domain.Amazon
+{
+    public class KeywordAnalysisService : IKeywordAnalysisService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<KeywordAnalysis> _repository;
+        private readonly IMapper _mapper;
+
+        public KeywordAnalysisService(
+            IUnitOfWork unitOfWork,
+            IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = unitOfWork.GetRepository<KeywordAnalysis>();
+            _mapper = mapper;
+        }
+
+        public KeywordAnalysisModel Get(long id)
+        {
+            KeywordAnalysis analysis = _repository.Get(id);
+
+            return _mapper.Map<KeywordAnalysisModel>(analysis);
+        }
+
+        public IList<KeywordAnalysisModel> GetAll()
+        {
+            IList<KeywordAnalysisModel> analyses = _mapper.Map<IList<KeywordAnalysisModel>>(_repository.GetAll());
+
+            return analyses;
+        }
+
+        public void Add(KeywordAnalysisModel analysis)
+        {
+            KeywordAnalysis analysisToAdd = new();
+
+            TransferValues(analysisToAdd, analysis);
+
+            _repository.Add(analysisToAdd);
+            _unitOfWork.Save();
+        }
+
+        public void Update(KeywordAnalysisModel analysis)
+        {
+            KeywordAnalysis analysisToUpdate = _repository.Get(analysis.Id);
+
+            TransferValues(analysisToUpdate, analysis);
+
+            _repository.Update(analysisToUpdate);
+            _unitOfWork.Save();
+        }
+
+        private void TransferValues(KeywordAnalysis toKeywordAnalysis, KeywordAnalysisModel fromKeywordAnalysis)
+        {
+            toKeywordAnalysis.Name = fromKeywordAnalysis.Name;
+            toKeywordAnalysis.Description = fromKeywordAnalysis.Description;
+            toKeywordAnalysis.InstanceId = fromKeywordAnalysis.Instance.Id;
+        }
+    }
+}
