bae59d5c0d08bd67dd8ea5c29260b69087a7e5d7
Kuba <jakub@mbetter.pl>
2021-11-13 12:25:21 +0100

    Cleaning solution from keyword analysis

--- MarketplaceBetter.Infrastructure/Data/BetterDbContext.cs
+++ MarketplaceBetter.Infrastructure/Data/BetterDbContext.cs
@@ -36,12 +36,6 @@ namespace MarketplaceBetter.Infrastructure.Data
         // Amazon
         public DbSet<AmazonParent> AmazonParent { get; set; }
         public DbSet<AmazonChild> AmazonChild { get; set; }
-        public DbSet<KeywordAnalysis> KeywordAnalysis { get; set; }
-        public DbSet<MainPhraseStatus> MainPhraseStatus { get; set; }
-        public DbSet<MainPhrase> MainPhrase { get; set; }
-        public DbSet<KeywordDataSource> KeywordDataSource { get; set; }
-        public DbSet<KeywordData> KeywordData { get; set; }
-        public DbSet<PhraseData> PhraseData { get; set; }
 
         //public override int SaveChanges()
         //{
