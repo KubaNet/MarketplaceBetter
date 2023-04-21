854cae2f1defae2cea190367d0242070dceb0e82
Jakub Sadkowski <kuba@mbetter.pl>
2023-04-21 13:07:24 +0200

    Saving product dimensions

--- MarketplaceBetter.Domain/Model/Catalog/Attributes/ProductDimensionsModel.cs
+++ MarketplaceBetter.Domain/Model/Catalog/Attributes/ProductDimensionsModel.cs
@@ -2,6 +2,7 @@
 using MarketplaceBetter.Domain.Model.Catalog.Products;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,10 @@ namespace MarketplaceBetter.Domain.Model.Catalog.Attributes
     {
         public long Id { get; set; }
 
+        [Required]
         public ProductModel Product { get; set; }
 
+        [Required]
         public SizeModel Size { get; set; }
 
         public double? WeightInGrams { get; set; }
