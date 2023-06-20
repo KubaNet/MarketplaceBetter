72d4f7b9a1c09b5cf394367e4892b8424408bf86
Jakub Sadkowski <kuba@mbetter.pl>
2023-06-20 12:42:29 +0200

    Reading ASIN from image file name

---
+++ MarketplaceBetter.Services/Specialized/Interfaces/IRefreshService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceBetter.Services.Specialized.Interfaces
+{
+    public interface IRefreshService
+    {
+        event Action RefreshRequested;
+
+        void RequestRefresh();
+    }
+}
---
+++ MarketplaceBetter.Services/Specialized/RefreshService.cs
@@ -0,0 +1,19 @@
+﻿using MarketplaceBetter.Services.Specialized.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceBetter.Services.Specialized
+{
+    public class RefreshService : IRefreshService
+    {
+        public event Action RefreshRequested;
+
+        public void RequestRefresh()
+        {
+            RefreshRequested?.Invoke();
+        }
+    }
+}
