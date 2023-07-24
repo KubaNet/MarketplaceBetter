251c04ebbb21335aa55eac0e2a8283b9ad3f5a0c
Jakub Sadkowski <kuba@mbetter.pl>
2023-07-24 14:53:53 +0200

    Product comments

---
+++ MarketplaceBetter.Services/Domain/Catalog/Products/Interfaces/IProductCommentService.cs
@@ -0,0 +1,18 @@
+﻿using MarketplaceBetter.Domain.Model.Catalog.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceBetter.Services.Domain.Catalog.Products.Interfaces
+{
+    public interface IProductCommentService
+    {
+        void Add(ProductModel product);
+
+        void Update(ProductModel product);
+
+        void Delete(ProductModel product);
+    }
+}
---
+++ MarketplaceBetter.Services/Domain/Catalog/Products/ProductCommentService.cs
@@ -0,0 +1,47 @@
+﻿using MarketplaceBetter.Domain.Entities.Catalog.Products;
+using MarketplaceBetter.Domain.Model.Catalog.Products;
+using MarketplaceBetter.Infrastructure.Data;
+using MarketplaceBetter.Services.Domain.Catalog.Products.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceBetter.Services.Domain.Catalog.Products
+{
+    public class ProductCommentService : IProductCommentService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<Product> _repository;
+
+        public ProductCommentService(
+            IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = unitOfWork.GetRepository<Product>();
+        }
+
+        public void Add(ProductModel product)
+        {
+            Update(product);
+        }
+
+        public void Update(ProductModel product)
+        {
+            Product productTo = _repository.Get(product.Id);
+
+            productTo.Comment = product.Comment;
+
+            _repository.Update(productTo);
+            _unitOfWork.Save();
+        }
+
+        public void Delete(ProductModel product)
+        {
+            product.Comment = null;
+
+            Update(product);
+        }
+    }
+}
