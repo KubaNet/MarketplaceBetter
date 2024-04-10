48dfd0fe4a2c79c5d39f28c1946edfa0b575f151
Jakub Sadkowski <kuba@mbetter.pl>
2024-04-10 19:18:56 +0200

    Custom SQL Scripts

---
+++ MarketplaceBetter.Database/Scripts/CustomScripts/DeletingCopywriting.sql
@@ -0,0 +1,2 @@
+﻿DELETE C FROM [dbo].[Copywriting] C JOIN Product P ON C.ProductId = P.Id JOIN CopywritingElement CE ON C.ElementId = CE.Id
+      WHERE P.BrandId = 1 and CE.SystemName != 1 and CE.SystemName != 2
---
+++ MarketplaceBetter.Database/Scripts/CustomScripts/InsertingCopywriting.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO [dbo].[Copywriting]
+           ([ProductId]
+           ,[InstanceId]
+           ,[ElementId]
+           ,[Value]
+           ,[ByteCount]
+           ,[HasProperLength])
+     SELECT
+			P.Id
+           ,I.Id
+           ,CE.Id
+           ,'GIVE A ELEGANT TOUCH to sleeves and a second life to your coat or jacket.'
+           ,DATALENGTH('GIVE A ELEGANT TOUCH to sleeves and a second life to your coat or jacket.')
+           ,case when (DATALENGTH('GIVE A ELEGANT TOUCH to sleeves and a second life to your coat or jacket.') < 200) then 1 else 0 end
+	FROM Product P, Instance I, CopywritingElement CE
+	WHERE P.Code in ('MC','MS') and I.Name in ('CA','UK','US') and CE.Name = 'BP1'
+GO
